570c7d9edaea0c9c2b3f695ad0d257dd68569a21
martindahl <dahlmartin@gmail.com>
2015-05-18 10:28:37 +0200

    ACF connected articles

--- wp-content/themes/ripples/templates/frontpage.php
+++ wp-content/themes/ripples/templates/frontpage.php
@@ -3,26 +3,6 @@
  * Template Name: Frontpage
  */
 ?>
-<style>
-    .embed-container {
-        position: relative;
-        padding-bottom: 56.25%;
-        height: 0;
-        overflow: hidden;
-        max-width: 100%;
-        height: auto;
-    }
-
-    .embed-container iframe,
-    .embed-container object,
-    .embed-container embed {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
-</style>
 
 <?php
 
@@ -31,6 +11,11 @@ if (have_rows('flexible_sections')):
 
     // loop through the rows of data
     while (have_rows('flexible_sections')) : the_row();
+
+        /****************************************************************************************
+         *
+         * Video
+         *****************************************************************************************/
 
         if (get_row_layout() == 'video'):?>
             <section class="front-page-section">
@@ -41,6 +26,11 @@ if (have_rows('flexible_sections')):
                 </div>
             </section>
         <?php
+        /****************************************************************************************
+         *
+         * What is oiid
+         *****************************************************************************************/
+
         elseif (get_row_layout() == 'what_is_oiid'):
             if (have_rows('item')):?>
                 <section class="front-page-section">
@@ -66,18 +56,47 @@ if (have_rows('flexible_sections')):
                     <?php endwhile; ?>
                 </section>
             <?php endif;
+        /****************************************************************************************
+         *
+         * Try it now
+         *****************************************************************************************/
 
         elseif (get_row_layout() == 'try_it_now'):?>
             <section class="front-page-section">
                 <h2><?php the_sub_field('title');?></h2>
                 <?php the_sub_field('text');?>
-                <?php  if (have_rows('share_buttons')):?>
-                    <?php while (have_rows('share_buttons')): the_row();?>
-                        <?php the_sub_field('url');?>
-                        <?php the_sub_field('button_image');?>
+                <?php if (have_rows('share_buttons')): ?>
+                    <?php while (have_rows('share_buttons')): the_row(); ?>
+                        <?php the_sub_field('url'); ?>
+                        <?php the_sub_field('button_image'); ?>
                     <?php endwhile; ?>
                 <?php endif;?>
 
+            </section>
+
+        <?php
+
+        /****************************************************************************************
+         *
+         * Connected articles
+         *****************************************************************************************/
+
+        elseif (get_row_layout() == 'connected_articles'):?>
+            <section class="front-page-section">
+                <?php $post_objects = get_sub_field('posts');
+
+                if ($post_objects): ?>
+                    <ul>
+                        <?php foreach ($post_objects as $post): // variable must be called $post (IMPORTANT) ?>
+                            <?php setup_postdata($post); ?>
+                            <li>
+                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                <?php the_post_thumbnail() ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+                <?php endif;?>
             </section>
 
         <?php endif;
