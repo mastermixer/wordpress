450c9005b41e4833686c6ebae99143db672935d7
martindahl <dahlmartin@gmail.com>
2015-05-18 12:48:27 +0200

    Deleted oiid-web, ACF front page stuff

--- wp-content/themes/ripples/templates/frontpage.php
+++ wp-content/themes/ripples/templates/frontpage.php
@@ -5,130 +5,157 @@
 ?>
 
 
-<section>
+<div class="page-top">
     <?php
-    $frontPageTopImage = get_field('front_page_top_background_image');?>
+    $frontPageTopImage = get_field('front_page_top_background_image'); ?>
     <picture>
-        <source media="(min-width: 64em)" src="<?php echo wp_get_attachment_image_src($frontPageTopImage, 'full')[0];?>">
-        <source media="(min-width: 37.5em)" src="<?php echo wp_get_attachment_image_src($frontPageTopImage)[0];?>">
-        <source src="<?php echo wp_get_attachment_image_src($frontPageTopImage)[0];?>">
-        <img src="<?php echo wp_get_attachment_image_src($frontPageTopImage)[0];?>" alt="This picture loads on non-supporting browsers.">
+        <source media="(min-width: 64em)"
+                src="<?php echo wp_get_attachment_image_src($frontPageTopImage, 'full')[0]; ?>">
+        <source media="(min-width: 37.5em)" src="<?php echo wp_get_attachment_image_src($frontPageTopImage)[0]; ?>">
+        <source src="<?php echo wp_get_attachment_image_src($frontPageTopImage)[0]; ?>">
+        <img src="<?php echo wp_get_attachment_image_src($frontPageTopImage)[0]; ?>"
+             alt="This picture loads on non-supporting browsers.">
+
         <p>Accessible text.</p>
     </picture>
-    <?php the_field('front_page_top_title');?>
-    <?php the_field('front_page_top_text');?>
-</section>
+    <?php the_field('front_page_top_title'); ?>
+    <?php the_field('front_page_top_text'); ?>
+</div>
 
+<div class="flexible-sections">
+    <?php
 
-<?php
+    // check if the flexible content field has rows of data
+    if (have_rows('flexible_sections')):
 
-// check if the flexible content field has rows of data
-if (have_rows('flexible_sections')):
+        // loop through the rows of data
+        while (have_rows('flexible_sections')) : the_row();
 
-    // loop through the rows of data
-    while (have_rows('flexible_sections')) : the_row();
+            /****************************************************************************************
+             *
+             * Video
+             *****************************************************************************************/
 
-        /****************************************************************************************
-         *
-         * Video
-         *****************************************************************************************/
-
-        if (get_row_layout() == 'video'):?>
-            <section class="front-page-section">
-                <h2><?php the_sub_field('video_title');?></h2>
-                <?php the_sub_field('video_text');?>
-                <div class="embed-container">
-                    <?php the_sub_field('video');?>
-                </div>
-            </section>
-        <?php
-        /****************************************************************************************
-         *
-         * What is oiid
-         *****************************************************************************************/
-
-        elseif (get_row_layout() == 'what_is_oiid'):
-            if (have_rows('item')):?>
+            if (get_row_layout() == 'video'):?>
                 <section class="front-page-section">
-                    <?php while (have_rows('item')): the_row();
-
-                        // vars
-                        $title = get_sub_field('title');
-                        $text = get_sub_field('text');
-                        $image = get_sub_field('image');
-
-                        ?>
-                        <div>
-                            <h2><?php echo $title; ?></h2>
-                            <?php echo $text; ?>
+                    <div class="inner-container">
+                        <div class="center-text">
+                            <h2><?php the_sub_field('video_title');?></h2>
+                            <?php the_sub_field('video_text');?>
                         </div>
-                        <div>
-                            <?php echo wp_get_attachment_image($image); ?>
+                        <div class="video-container ratio-16-9">
+                            <?php the_sub_field('video');?>
                         </div>
-
-                        <?php echo $content; ?>
-
-
-                    <?php endwhile; ?>
-                </section>
-            <?php endif;
-        /****************************************************************************************
-         *
-         * Try it now
-         *****************************************************************************************/
-
-        elseif (get_row_layout() == 'try_it_now'):?>
-            <section class="front-page-section">
-                <h2><?php the_sub_field('title');?></h2>
-                <?php the_sub_field('text');?>
-                <?php if (have_rows('share_buttons')): ?>
-                    <div class="share-buttons">
-                        <?php while (have_rows('share_buttons')): the_row(); ?>
-                            <?php $buttonImage = get_sub_field('button_image'); ?>
-                            <a href="<?php the_sub_field('url'); ?>">
-                                <?php echo wp_get_attachment_image($buttonImage); ?>
-                            </a>
-                        <?php endwhile; ?>
                     </div>
-                <?php endif;?>
-            </section>
+                </section>
+            <?php
+            /****************************************************************************************
+             *
+             * What is oiid
+             *****************************************************************************************/
 
-        <?php
+            elseif (get_row_layout() == 'what_is_oiid'):?>
+                <section class="front-page-section">
+                    <div class="inner-container">
+                        <div class="center-text">
+                            <h2><?php the_sub_field('what_is_title');?></h2>
+                            <?php the_sub_field('what_is_text');?>
+                        </div>
+                        <?php if (have_rows('item')): ?>
+                            <?php while (have_rows('item')): the_row();
 
-        /****************************************************************************************
-         *
-         * Connected articles
-         *****************************************************************************************/
+                                // vars
+                                $title = get_sub_field('title');
+                                $text = get_sub_field('text');
+                                $image = get_sub_field('image');
 
-        elseif (get_row_layout() == 'connected_articles'):?>
-            <section class="front-page-section">
-                <?php $post_objects = get_sub_field('posts');
+                                ?>
+                                <div class="what-is-item clearfix">
+                                    <div class="what-is-text">
+                                        <h3><?php echo $title; ?></h3>
+                                        <?php echo $text; ?>
+                                    </div>
+                                    <div class="what-is-image">
+                                        <?php echo wp_get_attachment_image($image); ?>
+                                    </div>
+                                </div>
 
-                if ($post_objects): ?>
-                    <ul class="post-list">
-                        <?php foreach ($post_objects as $post): // variable must be called $post (IMPORTANT) ?>
-                            <?php setup_postdata($post); ?>
-                            <li>
-                                <a href="<?php the_permalink(); ?>">
-                                    <?php the_title(); ?>
-                                    <?php the_post_thumbnail() ?>
-                                </a>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-                <?php endif;?>
-            </section>
 
-        <?php endif;
+                            <?php endwhile; ?>
 
-    endwhile;
+                        <?php endif;?>
+                    </div>
+                </section>
+            <?php
+            /****************************************************************************************
+             *
+             * Try it now
+             *****************************************************************************************/
 
-else :
+            elseif (get_row_layout() == 'try_it_now'):?>
+                <section class="front-page-section">
+                    <div class="inner-container">
+                        <div class="center-text">
+                            <h2><?php the_sub_field('title');?></h2>
+                            <?php the_sub_field('text');?>
+                        </div>
+                        <?php if (have_rows('share_buttons')): ?>
+                            <div class="share-buttons">
+                                <?php while (have_rows('share_buttons')): the_row(); ?>
+                                    <?php $buttonImage = get_sub_field('button_image'); ?>
+                                    <a href="<?php the_sub_field('url'); ?>">
+                                        <?php echo wp_get_attachment_image($buttonImage); ?>
+                                    </a>
+                                <?php endwhile; ?>
+                            </div>
+                        <?php endif;?>
+                    </div>
+                </section>
 
-    // no layouts found
+            <?php
 
-endif;
+            /****************************************************************************************
+             *
+             * Connected articles
+             *****************************************************************************************/
 
-?>
+            elseif (get_row_layout() == 'connected_articles'):?>
+                <section class="front-page-section">
+                    <div class="inner-container">
+                        <div class="center-text">
+                            <h2><?php the_sub_field('connected_title');?></h2>
+                            <?php the_sub_field('connected_text');?>
+                        </div>
+                        <?php $post_objects = get_sub_field('posts');
 
+                        if ($post_objects): ?>
+                            <ul class="post-list">
+                                <?php foreach ($post_objects as $post): // variable must be called $post (IMPORTANT) ?>
+                                    <?php setup_postdata($post); ?>
+                                    <li>
+                                        <a href="<?php the_permalink(); ?>">
+                                            <?php the_title(); ?>
+                                            <?php the_post_thumbnail() ?>
+                                        </a>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                            <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+                        <?php endif;?>
+                    </div>
+                </section>
+
+            <?php endif;
+
+        endwhile;
+
+    else :
+
+        // no layouts found
+
+    endif;
+
+    ?>
+
+
+</div>
