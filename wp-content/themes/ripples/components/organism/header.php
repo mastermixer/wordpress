5fafb71c84c1a230c8fe5adca2582dfae9836471
Øyvind Bø <oyvind.bo@makingwaves.no>
2015-05-18 12:46:49 +0200

    header stuff

--- wp-content/themes/ripples/components/organism/header.php
+++ wp-content/themes/ripples/components/organism/header.php
@@ -1,15 +1,16 @@
 <?php use MW\Ripples\Nav; ?>
 
 <header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+	<div class="inner-container">
 
-	<div id="logo" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/logo.svg" alt="<?php _e( 'To the frontpage', 'ripples' ); ?>" /></a></div>
+		<div id="logo" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/logo.svg" alt="<?php _e( 'To the frontpage', 'ripples' ); ?>" /></a></div>
 
-	<nav id="primary-navigation" class="navigation" class="main-menu" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-		<?php
-		if (has_nav_menu('main_menu')) :
-			wp_nav_menu(['theme_location' => 'main_menu', 'walker' => new Nav\SageNavWalker(), 'menu_class' => 'main-menu']);
-		endif;
-		?>
-	</nav>
-
+		<nav id="primary-navigation" class="navigation" class="main-menu" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+			<?php
+			if (has_nav_menu('main_menu')) :
+				wp_nav_menu(['theme_location' => 'main_menu', 'walker' => new Nav\SageNavWalker(), 'menu_class' => 'main-menu']);
+			endif;
+			?>
+		</nav>
+	</div>
 </header>
