a9f19a8522c78c20fdcd0081867e74bcb3ffc6d0
martindahl <dahlmartin@gmail.com>
2015-05-19 09:30:31 +0200

    Front page stuff

--- wp-content/themes/ripples/components/template_part/content-single.php
+++ wp-content/themes/ripples/components/template_part/content-single.php
@@ -1,17 +1,27 @@
-<?php inc( 'atom', 'main-start' ); ?>
+<?php inc('atom', 'main-start'); ?>
 <?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-	    <?php inc('molecule', 'entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
-    <footer>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'ripples'), 'after' => '</p></nav>']); ?>
-    </footer>
-	<?php inc('organism', 'comments'); ?>
-  </article>
+    <article <?php post_class(); ?>>
+        <header>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <?php inc('molecule', 'entry-meta'); ?>
+            content single
+        </header>
+
+        <div class="left-col">
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+        </div>
+
+        <div class="right-col">
+
+        </div>
+
+
+        <footer>
+            <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'ripples'), 'after' => '</p></nav>']); ?>
+        </footer>
+
+    </article>
 <?php endwhile; ?>
-<?php inc( 'atom', 'main-end' ); ?>
+<?php inc('atom', 'main-end'); ?>
