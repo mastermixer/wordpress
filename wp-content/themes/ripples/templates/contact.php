3cb7f0a9aff857950d8c489ec8adf7c8934de358
Øyvind Bø <oyvind.bo@makingwaves.no>
2015-05-19 12:02:31 +0200

    content singular code in contact us template

--- wp-content/themes/ripples/templates/contact.php
+++ wp-content/themes/ripples/templates/contact.php
@@ -4,4 +4,30 @@
  */
 ?>
 
-<h1>Contact us</h1>
+<?php inc('atom', 'main-start'); ?>
+<?php while (have_posts()) : the_post(); ?>
+    <article <?php post_class(); ?>>
+        <div class="inner-container">
+        <header>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <?php inc('molecule', 'entry-meta'); ?>
+        </header>
+
+        <div class="left-col">
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+        </div>
+
+        <div class="right-col">
+
+        </div>
+
+
+        <footer>
+            <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'ripples'), 'after' => '</p></nav>']); ?>
+        </footer>
+        </div>
+    </article>
+<?php endwhile; ?>
+<?php inc('atom', 'main-end'); ?>
