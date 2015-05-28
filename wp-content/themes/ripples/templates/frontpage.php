7dcbb5ac9e6115b015fa27d163330f7c094b8ea2
Øyvind Bø <oyvind.bo@makingwaves.no>
2015-05-28 14:02:02 +0200

    hamburger in menu

--- wp-content/themes/ripples/templates/frontpage.php
+++ wp-content/themes/ripples/templates/frontpage.php
@@ -20,11 +20,12 @@
 </div>
 
 <div class="front-page-nav" id="front-page-nav">
+    <div class="nav-button">
+    </div>
 
     <!-- Using js to populate this list with links check out frontPageMenu.js. Names are set in WP admin on the front page pr section -->
     <!--TODO: Needs styling and highlighting when active and is in view. Use Waypoints??-->
-    <ul></ul>
-
+    <ul class="nav-list"></ul>
     <div class="app-buttons">
 
         <?php while (have_rows('download_buttons', 'option')): the_row(); ?>
