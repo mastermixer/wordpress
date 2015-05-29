1acf3008ba6b27a6a2235a96ebcb1f459af5f707
martindahl <dahlmartin@gmail.com>
2015-05-29 09:26:59 +0200

    added waypoints to the front page menu for active states

--- wp-content/themes/ripples/assets/scripts/frontPageMenu.js
+++ wp-content/themes/ripples/assets/scripts/frontPageMenu.js
@@ -38,6 +38,38 @@
 
                     });
 
+                    var nav = $('.nav-list');
+                    var sections = document.getElementsByClassName('section');
+                    //Create different waypoints for scrolling up and down.
+                    for (var i = 0; i < sections.length; i++) {
+                        new Waypoint({
+                            element: sections[i],
+                            handler: function(direction) {
+                                console.log(this.element.id);
+
+                                if (direction === 'up') {
+                                    console.log('direction:'+direction);
+                                    nav.find('a').removeClass('active');
+                                    nav.find('a[href="#' + this.element.id + '"]').addClass('active');
+                                }
+                            },
+                            offset: 90
+                        });
+                        new Waypoint({
+                            element: sections[i],
+                            handler: function(direction) {
+                                console.log(this.element.id);
+
+                                if (direction === 'down') {
+                                    console.log('direction:'+direction);
+                                    nav.find('a').removeClass('active');
+                                    nav.find('a[href="#' + this.element.id + '"]').addClass('active');
+                                }
+                            },
+                            offset: '40%'
+                        });
+                    }
+
                     $('.nav-button').on('click', function() {
                         $('.nav-mobile-wrapper').toggleClass('visible');
                     });
