6ebb45c8bd467ba1ef9d55a2639ba7ad0e331666
Øyvind Bø <oyvind.bo@makingwaves.no>
2015-06-07 23:36:16 +0200

    44891-54: entry meta fix

--- wp-content/themes/ripples/components/molecule/entry-meta.php
+++ wp-content/themes/ripples/components/molecule/entry-meta.php
@@ -1,6 +1,6 @@
 <div class="entry-meta">
-    <p class="published"><?= __('entryMeta.published', 'ripples'); ?>
+    <p class="published"><?=  __('Published', 'ripples'); ?>
         <time class="updated" datetime="<?= get_the_time('c'); ?>"><?= get_the_date('j.m.Y \k\l. H:i'); ?></time>
     </p>
-    <p class="byline"><?= __('entryMeta.writtenBy', 'ripples'); ?> <?= get_the_author(); ?></p>
+    <p class="byline"><?= __('Written by', 'ripples'); ?> <?= get_the_author(); ?></p>
 </div>
