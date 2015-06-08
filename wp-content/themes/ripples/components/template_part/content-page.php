5564f6b54e45b21e3ceb95ab1f1ba02ce7eea134
Øyvind Bø <oyvind.bo@makingwaves.no>
2015-06-08 23:06:30 +0200

    44891-60: made template for page as post with a few tweaks

--- wp-content/themes/ripples/components/template_part/content-page.php
+++ wp-content/themes/ripples/components/template_part/content-page.php
@@ -1,5 +1,36 @@
-<div class="inner-container">
-    <?php inc('molecule', 'page-header'); ?>
-    <?php the_content(); ?>
-    <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'ripples'), 'after' => '</p></nav>']); ?>
-</div>
+
+<?php inc('atom', 'main-start'); ?>
+<article class="page-template-page">
+	<div class="page-top">
+	    <div class="page-top-wrapper">
+	        <div class="page-top-inside">
+			    <?php inc('molecule', 'page-header'); ?>
+	        </div>
+	    </div>
+	</div>
+
+	<div class="article-main">
+	    <div class="inner-container">
+	        <div class="left-col">
+	            <div class="entry-intro">
+	                <?php //the_excerpt(); ?>
+	            </div>
+	            <div class="entry-content">
+	                <?php the_content(); ?>
+	            </div>
+	        </div>
+
+	        <div class="right-col">
+	            <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'ripples'), 'after' => '</p></nav>']); ?>
+	        </div>
+
+	        <?php inc('molecule', 'social-media'); ?>
+	    </div>
+	</div>
+</article>
+
+<?php inc('atom', 'main-end'); ?>
+<?php inc('organism', 'explore-oiid'); ?>
+
+
+
