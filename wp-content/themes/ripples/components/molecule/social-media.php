fe02d90624f16a9a0c5f96dad11a5570445e9926
Øyvind Bø <oyvind.bo@makingwaves.no>
2015-06-09 11:15:51 +0200

    social media text in email

--- wp-content/themes/ripples/components/molecule/social-media.php
+++ wp-content/themes/ripples/components/molecule/social-media.php
@@ -19,7 +19,9 @@ $sharing_linkedin = sprintf("http://www.linkedin.com/shareArticle?title=%s&url=%
     urlencode($sharing_title),
     urlencode($sharing_link)
 );
-$sharing_email    = sprintf("mailto:?subject=Oiid is fun&body=%s",
+$sharing_email    = sprintf("mailto:?subject=oiid – Step Inside Music – Available in the App store.&body=Hi friend! You should check out oiid - A brand new way of exploring the music you love, layer by layer. Through intuitive app design you can listen to, mute, adjust or pan any instrument in a song. Curious about that bass line? Strip down everything else. Feel like singing along? Hit the lyrics button. Want to learn how to play? Check out the chords.
+We hope you’ll enjoy oiid as much as we do. And that you will help us create a new dimension to music.
+Learn more at oiid.com and like us on Facebook for more updates. %s",
     urlencode($sharing_link)
 );
 $sharing_googleplus    = sprintf("https://plus.google.com/share?url=%s",
